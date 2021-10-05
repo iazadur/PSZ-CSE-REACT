57b2a3689b00f001c1bc4d04ddd3247f424b25b2
iazadur <iamazadur@gmail.com>
2021-10-05 14:34:41 +0600

    add

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,18 +1,161 @@
 import { faMailBulk, faPhoneAlt } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
+import { CartesianGrid, Cell, Legend, Line, LineChart, Pie, PieChart, Tooltip, XAxis, YAxis } from 'recharts';
+
+
+
+const data = [
+    {
+        name: "Page A",
+        uv: 4000,
+        pv: 2400,
+        amt: 2400
+    },
+    {
+        name: "Page B",
+        uv: 3000,
+        pv: 1398,
+        amt: 2210
+    },
+    {
+        name: "Page C",
+        uv: 2000,
+        pv: 9800,
+        amt: 2290
+    },
+    {
+        name: "Page D",
+        uv: 2780,
+        pv: 3908,
+        amt: 2000
+    },
+    {
+        name: "Page E",
+        uv: 1890,
+        pv: 4800,
+        amt: 2181
+    },
+    {
+        name: "Page F",
+        uv: 2390,
+        pv: 3800,
+        amt: 2500
+    },
+    {
+        name: "Page G",
+        uv: 3490,
+        pv: 4300,
+        amt: 2100
+    }
+];
+
+
+const dataPie = [
+    { name: "Group A", value: 400 },
+    { name: "Group B", value: 300 },
+    { name: "Group C", value: 300 },
+    { name: "Group D", value: 200 }
+];
+
+const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
+
+const RADIAN = Math.PI / 180;
+const renderCustomizedLabel = ({
+    cx,
+    cy,
+    midAngle,
+    innerRadius,
+    outerRadius,
+    percent,
+    index
+}) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
+    return (
+        <text
+            x={x}
+            y={y}
+            fill="white"
+            textAnchor={x > cx ? "start" : "end"}
+            dominantBaseline="central"
+        >
+            {`${(percent * 100).toFixed(0)}%`}
+        </text>
+    );
+};
 
 const About = () => {
+
+
     return (
         <Container style={{ marginTop: "100px" }}>
+            <br /><br /><br />
+
+            <Row>
+                <Col>
+                    <PieChart width={400} height={400}>
+                        <Pie
+                            data={dataPie}
+                            cx={200}
+                            cy={200}
+                            labelLine={false}
+                            label={renderCustomizedLabel}
+                            outerRadius={180}
+                            fill="#8884d8"
+                            dataKey="value"
+                        >
+                            {dataPie.map((entry, index) => (
+                                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                            ))}
+                        </Pie>
+                    </PieChart>
+                </Col>
+
+
+                <Col>
+                    <LineChart
+                        width={700}
+                        height={400}
+                        data={data}
+                        margin={{
+                            top: 5,
+                            right: 30,
+                            left: 20,
+                            bottom: 5
+                        }}
+                    >
+                        <CartesianGrid strokeDasharray="3 3" />
+                        <XAxis dataKey="name" />
+                        <YAxis />
+                        <Tooltip />
+                        <Legend />
+                        <Line
+                            type="monotone"
+                            dataKey="pv"
+                            stroke="#8884d8"
+                            activeDot={{ r: 8 }}
+                        />
+                        <Line type="monotone" dataKey="uv" stroke="#82ca9d" />
+                    </LineChart>
+                </Col>
+            </Row>
+
+
+
+
+
+            <br /><br /><br />
             <div className="fw-normal text-muted mx-auto w-50  py-5  ">
-            <p>All of polytechnic students are benefited from this page. Because, lot of note and video class are provide here.In particular, Computer Department student are more benefited because Private Study Zone is a DUET Admission Coaching only for CSE
+                <p>All of polytechnic students are benefited from this page. Because, lot of note and video class are provide here.In particular, Computer Department student are more benefited because Private Study Zone is a DUET Admission Coaching only for CSE
                 </p>
                 <b><FontAwesomeIcon className="fs-6" icon={faMailBulk} />  engg.asmaul@gmail.com</b>\
                 <p><FontAwesomeIcon className="fs-6" icon={faPhoneAlt} /> <b>+8801719737799</b></p>
             </div>
-            
+
         </Container>
     );
 };
